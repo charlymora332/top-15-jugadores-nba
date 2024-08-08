279df003e8ced674767cd6c4a8890e544e10d4b6
Carlos Mora <charlymora332@gmail.com>
2024-08-07 23:01:48 -0500

    Archivos basicos primer commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://charlymora332.github.io/top-15-jugadores-nba"
 })
